16e565e5596be5d801d85975285414dfd4668d0d
Johnnie23919 <zhouwufan@foxmail.com>
2020-06-13 17:22:21 +0800

    create project

---
+++ src/main/java/com/xlous/demo/User.java
@@ -0,0 +1,11 @@
+package com.xlous.demo;
+
+/*
+ * @Description:
+ * @Author:Johnnie
+ * @DateTime: 2020/6/13 16:49
+ */
+public class User {
+    private String name;
+    private String password;
+}
